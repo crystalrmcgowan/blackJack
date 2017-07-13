f72851d360deea91963a05736625d5fb5fd15eac
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-13 16:18:00 -0400

    Final

--- scripts/main.js
+++ scripts/main.js
@@ -12,8 +12,39 @@
 console.log('loaded javascript');
 
 function handValue(hand) {
-  return
+  let handTotal = 0
+
+  for (var i = 0; i < hand.length; i++) {
+    if ((hand[i] != "J") && (hand[i] != "Q") && (hand[i] != "K") && (hand[i] != "A")) {
+      handTotal += parseInt(hand[i])
+    }
+  }
+
+  for (var i = 0; i < hand.length; i++) {
+    if ((hand[i] === "J") || (hand[i] === "Q") || (hand[i] === "K")){
+      handTotal += 10
+    }
+  }
+
+  for (var i = 0; i < hand.length; i++) {
+    if ((hand[i] === "A") && (handTotal + 11 > 21)) {
+      handTotal += 1
+    } else if ((hand[i] === "A") && (handTotal + 11 <= 21)){
+      handTotal += 11
+    }
+  }
+  return handTotal
 }
+
+// Goal: A function that can take any hand and return the total value of that hand.
+// Takes an array of cards as an argument and returns single number.
+
+
+
+
+
+
+
 
 /* -----  Hints ------
 
